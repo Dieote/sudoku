0279c692a6a85c914d90b22f1b64a0e54e4bc776
Dieote <gomezmdiego97@gmail.com>
2023-03-06 10:24:40 +0100

    modified specSudo

--- cypress/e2e/specSudo.cy.ts
+++ cypress/e2e/specSudo.cy.ts
@@ -1,8 +1,8 @@
 /// <reference types="cypress"/> 
-//Referencias de Cypress
+//References Cypress
 /*
 describe('Sudoku', () => {
-  it('Visitar Sudoku y autocompletar', () => {
+  it('Visit Sudoku and autofill', () => {
     cy.visit('/')
     //Total fills    
     cy.get('.game__cell.game__cell--filled').should('have.length', 45)
@@ -18,8 +18,39 @@ describe('Sudoku', () => {
 */
   describe('Sudoku', () => {
     it('Mods', () => {
-
+      //visit page
       cy.visit('/')
-
+      //confirm mode "easy"
+      cy.get('select[name=status__difficulty-select]').should('have.value','Easy')
+      //Total empty fills 
+      cy.get('.game__cell--filled').should('have.length', 45)
+      //change mode "medium"
+      cy.get('select[name=status__difficulty-select]').select('Medium')
+      //confirm empty fills
+      cy.get('.game__cell--filled').should('have.length', 40)
+      //change mode "hard"
+      cy.get('select[name=status__difficulty-select]').select('Hard')
+      //confirm empty fills
+      cy.get('.game__cell--filled').should('have.length', 30)
+      
   })  
+  describe('Sudoku', () => {
+    it('Shows diferent num of empty cells', () => {
+      //visit page
+      cy.visit('/')
+      cy.get('select[name=status__difficulty-select]').should('have.value','Easy')
+      cy.get('.game__cell--filled').should('have.length.greaterThan', 40).and('have.length.lessThan',81).its('length').as('easyN')
+
+      cy.get('select[name=status__difficulty-select]').select('Medium')
+      cy.get('.game__cell--filled').should('have.length.lessThan', 81).its('length').as('mediumN')
+
+      cy.get('select[name=status__difficulty-select]').select('Hard')
+      cy.get('.game__cell--filled').should('have.length.lessThan', 81).its('length').as('hardN')
+      .then( function () { //se rompio cuando puse function ?????
+        expect(this.easyN, 'easy').to.be.greaterThan(this.mediumN)
+        expect(this.mediumN, 'medium').to.be.greaterThan(this.hardN)
+      })
+
+    })
+  })
 })
