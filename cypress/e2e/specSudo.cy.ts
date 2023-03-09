79f7f38fea9861b4a6385299a4b1dac6e52a8c2a
Dieote <gomezmdiego97@gmail.com>
2023-03-09 12:15:50 +0100

    Cambios2

--- cypress/e2e/specSudo.cy.ts
+++ cypress/e2e/specSudo.cy.ts
@@ -21,31 +21,49 @@ describe('Sudoku', () => {
       //visit page
       cy.visit('/')
       //confirm mode "easy"
-      cy.get('select[name=status__difficulty-select]').should('have.value','Easy')
+      cy.get('select[name=status__difficulty-select]')
+      .should('have.value','Easy')
       //Total empty fills 
-      cy.get('.game__cell--filled').should('have.length', 45)
+      cy.get('.game__cell--filled')
+      .should('have.length', 45)
       //change mode "medium"
-      cy.get('select[name=status__difficulty-select]').select('Medium')
+      cy.get('select[name=status__difficulty-select]')
+      .select('Medium')
       //confirm empty fills
-      cy.get('.game__cell--filled').should('have.length', 40)
+      cy.get('.game__cell--filled')
+      .should('have.length', 40)
       //change mode "hard"
-      cy.get('select[name=status__difficulty-select]').select('Hard')
+      cy.get('select[name=status__difficulty-select]')
+      .select('Hard')
       //confirm empty fills
-      cy.get('.game__cell--filled').should('have.length', 30)
+      cy.get('.game__cell--filled')
+      .should('have.length', 30)
       
   })  
   describe('Sudoku', () => {
     it('Shows diferent num of empty cells', () => {
       //visit page
       cy.visit('/')
-      cy.get('select[name=status__difficulty-select]').should('have.value','Easy')
-      cy.get('.game__cell--filled').should('have.length.greaterThan', 40).and('have.length.lessThan',81).its('length').as('easyN')
+      cy.get('select[name=status__difficulty-select]')
+      .should('have.value','Easy')
+      
+      cy.get('.game__cell--filled')
+      .should('have.length.greaterThan', 40)
+      .and('have.length.lessThan',81)
+      .its('length')
+      .as('easyN')
 
-      cy.get('select[name=status__difficulty-select]').select('Medium')
-      cy.get('.game__cell--filled').should('have.length.lessThan', 81).its('length').as('mediumN')
+      cy.get('select[name=status__difficulty-select]')
+      .select('Medium')
+      cy.get('.game__cell--filled')
+      .should('have.length.lessThan', 81)
+      .its('length').as('mediumN')
 
-      cy.get('select[name=status__difficulty-select]').select('Hard')
-      cy.get('.game__cell--filled').should('have.length.lessThan', 81).its('length').as('hardN')
+      cy.get('select[name=status__difficulty-select]')
+      .select('Hard')
+      cy.get('.game__cell--filled')
+      .should('have.length.lessThan', 81)
+      .its('length').as('hardN')
       .then( function () { //se rompio cuando puse function ?????
         expect(this.easyN, 'easy').to.be.greaterThan(this.mediumN)
         expect(this.mediumN, 'medium').to.be.greaterThan(this.hardN)
